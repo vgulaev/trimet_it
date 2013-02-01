fac36e0512ac76b887b706aa64abda01dc65ba3f
vgulaev <vgulaev@yandex.ru>
2013-02-01 18:38:28 +0600

    Commit for commit

--- js/githubaccess.js
+++ js/githubaccess.js
@@ -1,4 +1,8 @@
-﻿function creategoogletable(issues) {
+﻿var str_i18n = function() {
+	//title = {""};
+};
+
+function creategoogletable(issues) {
 	/*google.load('visualization', '1', {packages:['table',]});
 	google.setOnLoadCallback(drawTable);*/
 	//function drawTable() {
@@ -45,4 +49,52 @@ function createtable() {
 		}
 	};
 	$.ajax(apiParams);
+}
+
+function creategoogletable_commits(commits) {
+
+	for ( var i in commits.data) {
+		d = new Date(Date.parse(commits.data[i].commit.author.date));
+		d.setHours(0);
+		d.setMinutes(0);
+		d.setSeconds(0);
+		d.setMilliseconds(0);
+		data2.addRow([ d, commits.data[i].commit.author.name]);
+	}
+
+	if (commits.meta.hasOwnProperty("Link")) {
+		if (commits.meta.Link[0][1].rel == "next") {
+			alert("next");
+			makepagingajax(commits.meta.Link[0][0]);
+	var table = new google.visualization.Table(document
+			.getElementById('table_div'));
+	table.draw(data2, {
+		showRowNumber : true
+	});
+		}
+	}
+}
+
+function makepagingajax(_loc) {
+	if (_loc == undefined) {
+		_loc = 'https://api.github.com/repos/vgulaev/sitenewwave/commits';
+		//_loc = 'https://api.github.com/repos/vgulaev/trimet_it/issues';
+	}
+	apiParams = {
+		url : _loc,
+		dataType : 'jsonp',
+		success : function(commits) {
+			creategoogletable_commits(commits);
+		}
+	};
+	$.ajax(apiParams);
+}
+
+function table_with_working_day() {
+	data2 = new google.visualization.DataTable();
+	data2.addColumn('date', 'День работы');
+	data2.addColumn('string', 'Работник');
+
+	makepagingajax();
+
 }
